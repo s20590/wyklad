f4fc033e50d534345eb22c8502e9fe0b0984170d
Jakub Jablonski <s20590@pjwstk.edu.pl>
2019-10-25 10:51:55 +0200

    od nowa kalk xd

--- main.cpp
+++ main.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-	cout<<"Ficzer"<<endl;
+int main() {
+	int a, b;
+	
+	cout<<"Wszytywanie liczb a, b:"<<endl;
+	cin>>a>>b;
+	
+	//Dodawanie 
+	
+	//Odejmowanie
+	
+	//Mnozenie
+	
+	//Dzielenie
+	
 	return 0;
 }
